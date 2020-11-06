fec323d3f62cc212b2c6471b7dafad764d2a2acf
Luke Dias <lhollowwizard@gmail.com>
2020-11-06 18:56:33 -0300

    Added the create new article page, but didn't fully implement it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,17 @@ class ArticlesController < ApplicationController
   def show
     @article  = Article.find(params[:id])
   end
+
+  def index
+    @articles  = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    render plain: params[:article]
+  end
+
 end
