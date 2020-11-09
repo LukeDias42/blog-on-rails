c0d4be1646a905c91bbb4d1c33ffeef5c3f724d1
Luke Dias <lhollowwizard@gmail.com>
2020-11-09 17:27:52 -0300

    Migrated the User Database

--- db/migrate/20201109201251_create_users.rb
+++ db/migrate/20201109201251_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.timestamp
+      t.timestamps
     end
   end
 end
