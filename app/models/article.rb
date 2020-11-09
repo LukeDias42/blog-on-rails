3a468802417686830c95c61bece4137ae6c15610
Luke Dias <lhollowwizard@gmail.com>
2020-11-09 11:09:01 -0300

    Used bootstrap on all the pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
