65aca0d7b0876b08263b7271744ff7bd1f023390
Luke Dias <lhollowwizard@gmail.com>
2020-11-10 06:16:29 -0300

    Added validations, including REGEX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 3, manimun: 25}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/iv
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
 end
