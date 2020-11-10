143bebe4bf2d966339290398a09b1669b768e373
Luke Dias <lhollowwizard@gmail.com>
2020-11-10 08:33:21 -0300

    User has password_digest column with has_secure_password from BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+
   has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
+
+  has_secure_password
 end
