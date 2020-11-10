fcaf8bfa94142b31587e313c1d5d32aaea0d475d
Luke Dias <lhollowwizard@gmail.com>
2020-11-10 13:35:54 -0300

    Added Login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
 
-  resources :users
+  resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
