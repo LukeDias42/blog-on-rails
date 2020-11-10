9c3201fe98db83a54c79cda15abdb33c8dbf01c5
Luke Dias <lhollowwizard@gmail.com>
2020-11-10 09:17:10 -0300

    Completed sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You signed up succesfully!"
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+
+  resources :users
+  get 'signup', to: 'users#new'
 end
