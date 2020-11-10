947354d9c1d957edeac719810690d95176dcbc6f
Luke Dias <lhollowwizard@gmail.com>
2020-11-10 07:43:56 -0300

    One User to many Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 4, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 3, manimun: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/iv
