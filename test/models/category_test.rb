eaab8655500bf4a1fba4e0a37ab5f9b701d7a3ee
Luke Dias <lhollowwizard@gmail.com>
2020-11-11 15:20:57 -0300

    Validated category using unit tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: "Sports")
+  end
 
   test "category should be valid" do
-    @category = Category.new(name: "Sports")
     assert @category.valid?
   end
 
-end
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 27
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "AA"
+    assert_not @category.valid?
+  end
+
+end 
