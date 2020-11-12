f65585c1369e4553faf6dedd4fd19fd4be91765f
Luke Dias <lhollowwizard@gmail.com>
2020-11-12 10:06:21 -0300

    Category creating page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,23 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+   
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "The category has been saved successfully"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
-
   end
 
   def show
-
   end
-  
+
 end
